65b1ccdfead1214f4a2ba39ff377f616d573bff6
Heidi <discreteneko@outlook.com>
2018-03-16 14:19:39 +0800

    Add project content

---
+++ src/main/java/Mutation.java
@@ -0,0 +1,8 @@
+public class Mutation {
+     public boolean isPositive(int a) {
+         if ((a >= 0) && (a < 101)) {
+             return true;
+         }
+         return false;
+     }
+}
---
+++ src/test/java/MutationTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+public class MutationTest {
+
+    @Test
+    public void testIsPositive() throws Exception {
+        Mutation sut = new Mutation();
+
+        assertTrue(sut.isPositive(1));
+        assertTrue(sut.isPositive(0));
+        assertTrue(sut.isPositive(100));
+
+        assertFalse(sut.isPositive(-1));
+        assertFalse(sut.isPositive(102));
+
+        //assertFalse(sut.isPositive(101));
+    }
+}
